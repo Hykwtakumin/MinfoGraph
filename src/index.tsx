98d3ccf63ea8f280b2e740e3a5a8ca7129ac814f
hykwtakumin <hykwtakumin@gmail.com>
2020-03-08 16:58:27 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { FC } from "react";
+import { render } from "react-dom";
+
+render(
+  <>
+    <div>{`This is Index Page!!`}</div>
+  </>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const webpack = require("webpack");
+const TsConfigWebpackPlugin = require("ts-config-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const path = require("path");
+const nodeEnv = process.env.NODE_ENV;
+
+const plugins = [
+new TsConfigWebpackPlugin(),
+new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+];
+
+if (nodeEnv === "stats") plugins.push(new BundleAnalyzerPlugin());
+
+module.exports = {
+mode: process.env.NODE_ENV === "production" ? "production" : "development",
+entry: path.resolve(__dirname, "src/index.tsx"),
+output: {
+filename: "index.js",
+path: path.resolve(__dirname, "docs/dist/")
+},
+watch: nodeEnv !== "production",
+plugins,
+resolve: {
+alias: {
+"moment-with-locale": path.resolve(
+__dirname,
+"src/client/utils/momentWithLocale.ts"
+)
+}
+}
+};
